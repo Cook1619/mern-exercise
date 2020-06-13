1950ea1fe2ebe00c2395093d30e42296a725d089
Cook1619 <cook6348@gmail.com>
2020-06-12 20:49:59 -0500

    Setting up models and routers

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const moongoose =  require('mongoose');
+const exerciseRouter =  require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
 require('dotenv').config();
 
@@ -8,6 +11,17 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+moongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true}
+);
+const connection =  moongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection esatablished successfully');
+})
+
+app.use('/exercises', exerciseRouter);
+app.use('users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
