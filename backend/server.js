a3c24b9b5206eba0707e3ae0f3bf41663fc783c9
Cook1619 <cook6348@gmail.com>
2020-06-12 21:27:16 -0500

    Finished toy app

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-moongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true}
+moongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection =  moongoose.connection;
 connection.once('open', () => {
